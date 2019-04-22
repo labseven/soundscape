f853bf2d09b004dbc286d2f3e6f4b0fb5c820d8b
SeunginLyu <seungin.lyu@students.olin.edu>
2019-04-22 14:22:16 -0400

    Update for four stations

--- rasp_pi/soundscape.py
+++ rasp_pi/soundscape.py
@@ -16,48 +16,52 @@ button2 = digitalio.DigitalInOut(board.D24)
 button2.direction = digitalio.Direction.INPUT
 button2.pull = digitalio.Pull.UP
 
-# button3 = digitalio.DigitalInOut(board.D25)
-# button3.direction = digitalio.Direction.INPUT
-# button3.pull = digitalio.Pull.UP
+button3 = digitalio.DigitalInOut(board.D25)
+button3.direction = digitalio.Direction.INPUT
+button3.pull = digitalio.Pull.UP
+
+button4 = digitalio.DigitalInOut(board.D26)
+button4.direction = digitalio.Direction.INPUT
+button4.pull = digitalio.Pull.UP
 
 mp3_files = [ f for f in listdir('../assets/') if f[-4:] == '.mp3' ]
 
 
 if not len(mp3_files) > 0:
     print("No mp3 files found!")
+else:
+    print('--- Available mp3 files ---')
+    print(mp3_files)
 
-print('--- Available mp3 files ---')
-print(mp3_files)
+isPlaying = 0
 
-isPlaying = [False, False]
-# btnList = [button1, button2]
+def processPlugIn(buttonIndex):
+    if(isPlaying is not buttonIndex):
+            subprocess.call(['killall', 'omxplayer.bin'])
+            subprocess.Popen(['omxplayer', '-o', 'local', '../assets/'+ mp3_files[buttonIndex-1]])
+            isPlaying = buttonIndex
+        print('--- Playing ' + mp3_files[buttonIndex-1] + ' ---')
+        time.sleep(0.25)
 
-# def resetButtonValuesExcept(exceptButton):
-#     for button in btnList:
-#         if button != exceptButton:
-#             button.value = True
-#             print("resetting %s", button)
-#     subprocess.call(['killall', 'omxplayer'])
-#     print('--- Cleared all existing mp3s. ---')
-
+def processPlugOut(buttonIndex):
+    subprocess.call(['killall', 'omxplayer.bin'])
+    isPlaying = 0
+    time.sleep(0.25)
+    print('--- Stopping ' + mp3_files[buttonIndex-1] + ' ---')
+    
 while True:
-
+    # if the user plugs-out
+    if button1.value or button2.value or button3.value or button4.value and isPlaying == 0:
+        processPlugOut()
+    # if the user plugs-in
     if not button1.value:
-        if(not isPlaying[0]):
-            subprocess.call(['killall', 'omxplayer.bin'])
-            subprocess.Popen(['omxplayer', '-o', 'local', '../assets/'+ mp3_files[0]])
-            isPlaying[0] = True
-            isPlaying[1] = False
-        print('--- Playing ' + mp3_files[0] + ' ---')
-        time.sleep(0.25)
-
+        processPlugIn(1);
     if not button2.value:
-        if(not isPlaying[1]):
-            subprocess.call(['killall', 'omxplayer.bin'])
-            subprocess.Popen(['omxplayer', '-o', 'local', '../assets/'+ mp3_files[1]])
-            isPlaying[1] = True
-            isPlaying[0] = False
-        print('--- Playing ' + mp3_files[1] + ' ---')
-        time.sleep(0.25)
+        processPlugIn(2);
+    if not button3.value:
+        processPlugIn(3);
+    if not button4.value:
+        processPlugIn(4); 
 
+    # if the user plugs-out       
     time.sleep(0.25)
