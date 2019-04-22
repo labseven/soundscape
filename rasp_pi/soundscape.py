cc8cce9d5696a34f3a69bc44b8acda2637cdbb74
labseven <adam.novotny55@gmail.com>
2019-04-22 20:07:11 +0000

    Update soundscape.py

--- rasp_pi/soundscape.py
+++ rasp_pi/soundscape.py
@@ -8,21 +8,13 @@ import subprocess
 import board
 import digitalio
 
-button1 = digitalio.DigitalInOut(board.D22)
-button1.direction = digitalio.Direction.INPUT
-button1.pull = digitalio.Pull.UP
+button_pins = [board.D22, board.D23, board.D24, board.D27]
+buttons = []
 
-button2 = digitalio.DigitalInOut(board.D23)
-button2.direction = digitalio.Direction.INPUT
-button2.pull = digitalio.Pull.UP
-
-button3 = digitalio.DigitalInOut(board.D24)
-button3.direction = digitalio.Direction.INPUT
-button3.pull = digitalio.Pull.UP
-
-button4 = digitalio.DigitalInOut(board.D27)
-button4.direction = digitalio.Direction.INPUT
-button4.pull = digitalio.Pull.UP
+for pin in pins:
+    buttons.append(digitalio.DigitalInOut(pin))
+    buttons[-1].direction = digitalio.Direction.INPUT
+    buttons[-1].pull = digitalio.Pull.UP
 
 mp3_files = [ f for f in listdir('../assets/') if f[-4:] == '.mp3' ]
 
@@ -33,7 +25,7 @@ else:
     print('--- Available mp3 files ---')
     print(mp3_files)
 
-isPlaying = 0
+isPlaying = -1
 
 def processPlugIn(buttonIndex):
     global isPlaying
@@ -48,28 +40,15 @@ def processPlugOut(buttonIndex):
     global isPlaying
     subprocess.call(['killall', 'omxplayer.bin'])
     print('--- Stopping ' + mp3_files[buttonIndex-1] + ' ---')
-    isPlaying = 0
+    isPlaying = -1
     time.sleep(0.25)
 
 while True:
-    # if the user plugs-out
-    if button1.value and isPlaying == 1:
-        processPlugOut(1)
-    if button2.value and isPlaying == 2:
-        processPlugOut(2)
-    if button3.value and isPlaying == 3:
-        processPlugOut(3)
-    if button4.value and isPlaying == 4:
-        processPlugOut(4)
-    # if the user plugs-in
-    if (not button1.value) and isPlaying != 1:
-        processPlugIn(1);
-    if (not button2.value) and isPlaying != 2:
-        processPlugIn(2);
-    if (not button3.value) and isPlaying != 3:
-        processPlugIn(3);
-    if (not button4.value) and isPlaying != 4:
-        processPlugIn(4); 
-
-    # if the user plugs-out       
+    if isPlaying == -1:
+        for i, button in enumerate(buttons):
+            if (not button.value):
+                processPlugIn(i) 
+    else if buttons[isPlaying].value:
+        processPlugOut(i)
+      
     time.sleep(0.25)
