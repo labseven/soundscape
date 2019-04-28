00d4380d3b0528e6d26600787edc0d20f9417773
SeunginLyu <seungin.lyu@students.olin.edu>
2019-04-28 16:04:19 -0400

    Add pygame

--- rasp_pi/soundscape.py
+++ rasp_pi/soundscape.py
@@ -4,51 +4,74 @@
 
 import time
 from os import listdir
-import subprocess
-import board
-import digitalio
+import os
+# import board
+# import digitalio
+import pygame
 
-button_pins = [board.D22, board.D23, board.D24, board.D27]
+# button_pins = [board.D22, board.D23, board.D24, board.D27]
 buttons = []
 
-for pin in button_pins:
-    buttons.append(digitalio.DigitalInOut(pin))
-    buttons[-1].direction = digitalio.Direction.INPUT
-    buttons[-1].pull = digitalio.Pull.UP
+# for pin in button_pins:
+#     buttons.append(digitalio.DigitalInOut(pin))
+#     buttons[-1].direction = digitalio.Direction.INPUT
+#     buttons[-1].pull = digitalio.Pull.UP
 
-mp3_files = [ f for f in listdir('../assets/') if f[-4:] == '.mp3' ]
+ogg_files = [ f for f in listdir('../assets/') if f[-4:] == '.ogg' ]
 
-
-if not len(mp3_files) > 0:
-    print("No mp3 files found!")
+if not len(ogg_files) > 0:
+    print("No ogg files found!")
 else:
-    print('--- Available mp3 files ---')
-    print(mp3_files)
+    print('--- Available ogg files ---')
+    print(ogg_files)
+
+dirname = os.path.dirname(__file__)
+# init mixer and load sounds
+
+pygame.mixer.init()
+# pygame Sounds Object has limited mp3 support, using ogg files for everything
+print("Loading sounds file..")
+start = time.time()
+# ordered from right to left, from T-station to School
+sounds =[pygame.mixer.Sound(os.path.join(dirname, '../assets/barber_shop.ogg')),
+        pygame.mixer.Sound('../assets/walking.ogg'),
+        pygame.mixer.Sound('../assets/park.ogg'),
+        pygame.mixer.Sound('../assets/bakery.ogg'),
+        pygame.mixer.Sound('../assets/barber_shop.ogg'),
+        pygame.mixer.Sound('../assets/birds.ogg'),
+        pygame.mixer.Sound('../assets/kids.ogg')
+        ]
+end = time.time()
+print("Loading sounds complete, Elapsed Time :", end - start )
+
+
+# pygame.mixer.Channel(0).play(sounds[0])
+
 
 isPlaying = -1
 
-def processPlugIn(buttonIndex):
-    global isPlaying
-    subprocess.call(['killall', 'omxplayer.bin'])
-    subprocess.Popen(['omxplayer', '-o', 'local', '/home/pi/Documents/soundscape/assets/'+ mp3_files[buttonIndex]])
-    isPlaying = buttonIndex
-    print('--- Playing ' + mp3_files[buttonIndex] + ' ---')
-    time.sleep(0.25)
+# def processPlugIn(buttonIndex):
+#     global isPlaying
+#     subprocess.call(['killall', 'omxplayer.bin'])
+#     subprocess.Popen(['omxplayer', '-o', 'local', '/home/pi/Documents/soundscape/assets/'+ mp3_files[buttonIndex]])
+#     isPlaying = buttonIndex
+#     print('--- Playing ' + ogg_files[buttonIndex] + ' ---')
+#     time.sleep(0.25)
 
-def processPlugOut(buttonIndex):
-    global isPlaying
-    print('--- plugged out button : ', buttonIndex)
-    subprocess.call(['killall', 'omxplayer.bin'])
-    print('--- Stopping ' + mp3_files[buttonIndex] + ' ---')
-    isPlaying = -1
-    time.sleep(0.25)
+# def processPlugOut(buttonIndex):
+#     global isPlaying
+#     print('--- plugged out button : ', buttonIndex)
+#     subprocess.call(['killall', 'omxplayer.bin'])
+#     print('--- Stopping ' + ogg_files[buttonIndex] + ' ---')
+#     isPlaying = -1
+#     time.sleep(0.25)
 
-while True:
-    if isPlaying == -1:
-        for i, button in enumerate(buttons):
-            if button.value:
-                processPlugIn(i) 
-    elif not buttons[isPlaying].value:
-        processPlugOut(isPlaying)
+# while True:
+#     if isPlaying == -1:
+#         for i, button in enumerate(buttons):
+#             if button.value:
+#                 processPlugIn(i) 
+#     elif not buttons[isPlaying].value:
+#         processPlugOut(isPlaying)
         
-    time.sleep(0.25)
+#     time.sleep(0.25)
