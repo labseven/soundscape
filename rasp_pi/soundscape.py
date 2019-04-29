2199813c44cc6109ae4ef58e92c7c2b90fcf53fd
SeunginLyu <seungin.lyu@students.olin.edu>
2019-04-28 22:45:50 -0400

    Update code

--- rasp_pi/soundscape.py
+++ rasp_pi/soundscape.py
@@ -5,42 +5,37 @@
 import time
 from os import listdir
 import os
-# import board
-# import digitalio
+import board
+import digitalio
 import pygame
 
-# button_pins = [board.D22, board.D23, board.D24, board.D27]
+button_pins = [board.D2, board.D3, board.D4, board.D17, board.D23, board.D24]
 buttons = []
 
-# for pin in button_pins:
-#     buttons.append(digitalio.DigitalInOut(pin))
-#     buttons[-1].direction = digitalio.Direction.INPUT
-#     buttons[-1].pull = digitalio.Pull.UP
+for pin in button_pins:
+    buttons.append(digitalio.DigitalInOut(pin))
+    buttons[-1].direction = digitalio.Direction.INPUT
+    buttons[-1].pull = digitalio.Pull.UP
 
-ogg_files = [ f for f in listdir('../assets/') if f[-4:] == '.ogg' ]
+wav_files = [ f for f in listdir('../assets/') if f[-4:] == '.wav' ]
 
-if not len(ogg_files) > 0:
-    print("No ogg files found!")
+if not len(wav_files) > 0:
+    print("No wav files found!")
 else:
     print('--- Available ogg files ---')
-    print(ogg_files)
+    print(wav_files)
 
 dirname = os.path.dirname(__file__)
 # init mixer and load sounds
 
 pygame.mixer.pre_init(44100, -16, 2, 2048)
 pygame.mixer.init()
-# pygame Sounds Object has limited mp3 support, using ogg files for everything
+# pygame Sounds Object has limited mp3 support, using wav files for everything
 print("Loading sounds file..")
 start = time.time()
 # ordered from right to left, from T-station to School
 sounds = []
-print("Loading barber_shop..")
-sounds.append(pygame.mixer.Sound('../assets/barber_shop.wav'))
-print("Loading birds..")
-sounds.append(pygame.mixer.Sound('../assets/birds.wav'))
-print("Loading kids..")
-sounds.append(pygame.mixer.Sound('../assets/kids.wav'))
+
 print("Loading t_station..")
 sounds.append(pygame.mixer.Sound('../assets/t_station.wav'))
 print("Loading walking..")
@@ -49,6 +44,14 @@ print("Loading park..")
 sounds.append(pygame.mixer.Sound('../assets/park.wav'))
 print("Loading bakery..")
 sounds.append(pygame.mixer.Sound('../assets/bakery.wav'))
+print("Loading barber_shop..")
+sounds.append(pygame.mixer.Sound('../assets/barber_shop.wav'))
+
+# not enough memory on rasp_pi
+# print("Loading birds..")
+# sounds.append(pygame.mixer.Sound('../assets/birds.wav'))
+# print("Loading kids..")
+# sounds.append(pygame.mixer.Sound('../assets/kids.wav'))
 
 
 end = time.time()
@@ -60,28 +63,27 @@ print("Loading sounds complete, Elapsed Time :", end - start )
 
 isPlaying = -1
 
-# def processPlugIn(buttonIndex):
-#     global isPlaying
-#     subprocess.call(['killall', 'omxplayer.bin'])
-#     subprocess.Popen(['omxplayer', '-o', 'local', '/home/pi/Documents/soundscape/assets/'+ mp3_files[buttonIndex]])
-#     isPlaying = buttonIndex
-#     print('--- Playing ' + ogg_files[buttonIndex] + ' ---')
-#     time.sleep(0.25)
+def processPlugIn(buttonIndex):
+    global isPlaying
+    pygame.mixer.Channel(buttonIndex).play(sounds[buttonIndex])
+    isPlaying = buttonIndex
+    print('--- Playing ' + wav_files[buttonIndex] + ' ---')
+    time.sleep(0.25)
 
-# def processPlugOut(buttonIndex):
-#     global isPlaying
-#     print('--- plugged out button : ', buttonIndex)
-#     subprocess.call(['killall', 'omxplayer.bin'])
-#     print('--- Stopping ' + ogg_files[buttonIndex] + ' ---')
-#     isPlaying = -1
-#     time.sleep(0.25)
+def processPlugOut(buttonIndex):
+    global isPlaying
+    print('--- plugged out button : ', buttonIndex)
+    pygame.mixer.stop()
+    print('--- Stopping ' + wav_files[buttonIndex] + ' ---')
+    isPlaying = -1
+    time.sleep(0.25)
 
-# while True:
-#     if isPlaying == -1:
-#         for i, button in enumerate(buttons):
-#             if button.value:
-#                 processPlugIn(i) 
-#     elif not buttons[isPlaying].value:
-#         processPlugOut(isPlaying)
+while True:
+    if isPlaying == -1:
+        for i, button in enumerate(buttons):
+            if button.value:
+                processPlugIn(i) 
+    elif not buttons[isPlaying].value:
+        processPlugOut(isPlaying)
         
-#     time.sleep(0.25)
+    time.sleep(0.25)
